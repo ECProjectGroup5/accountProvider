f577c305a6452684576a65c4609083107b666e20
Helga Reesalu <helga.reesalu@gmail.com>
2024-11-01 13:30:14 +0100

    Configured Identity

--- accountProvider/Program.cs
+++ accountProvider/Program.cs
@@ -1,5 +1,25 @@
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+
+builder.Services.AddDefaultIdentity<DataContext>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.SignIn.RequireConfirmedAccount = false;
+    x.Password.RequiredLength = 8;
+
+}).AddEntityFrameworkStores<DataContext>();
+builder.Services.ConfigureApplicationCookie(x =>
+{
+    x.LoginPath = "/signin";
+});
+
 
 var app = builder.Build();
 app.UseHttpsRedirection();
