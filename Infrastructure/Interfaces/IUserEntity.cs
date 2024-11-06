4a2b9a39708ff34733813bcb4f6b8764c4121e23
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-11-06 20:31:16 +0100

    Added SignUpViewModel and SignUp() in AuthController.

---
+++ Infrastructure/Interfaces/IUserEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Interfaces
+{
+    
+    /// <summary>
+    /// Represents a user entity that adds details to IdentityUser.
+    /// </summary>
+    /// <remarks>
+    /// Contains properties for first name, last name, and a check if the account is external.
+    /// /// </remarks>
+    public interface IUserEntity
+    {
+        string FirstName { get; set; }
+        bool IsExternalAccount { get; set; }
+        string LastName { get; set; }
+    }
+}
---
+++ accountProvider/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace accountProvider.ViewModels;
+
+/// <summary>
+/// 
+/// </summary>
+public class SignUpViewModel
+{
+	public string Email { get; set; } = null!;
+
+	public string Password { get; set; } = null!;
+
+	public bool TermsAndConditions { get; set; }
+}
