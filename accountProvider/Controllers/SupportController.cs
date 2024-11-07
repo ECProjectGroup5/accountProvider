ef4623ecd10d873873e3abb23ecc4a3233aafb46
Sotis11K <89807156+Sotis11K@users.noreply.github.com>
2024-11-07 12:29:21 +0100

    test

---
+++ accountProvider/Controllers/SupportController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace accountProvider.Controllers
+{
+    public class SupportController : Controller
+    {
+        [Route("/supporttelephone")]
+        public IActionResult SupportTelephone()
+        {
+            return View();
+        }
+    }
+}
